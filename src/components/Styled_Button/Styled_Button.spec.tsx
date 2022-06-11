b2d288f7eb0b606cfcd19cc46c58c373c8733867
SVA2021 <sofronov.vit@gmail.com>
2022-06-11 17:18:09 +0400

    upd: add test for styled button

---
+++ src/components/Styled_Button/Styled_Button.spec.tsx
@@ -0,0 +1,40 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import StyledButton from './Styled_Button';
+
+describe('render logo', () => {
+
+	const handleClicker = jest.fn();
+
+	const setUp = () => render(
+		<StyledButton variant="contained" aria-label="hello button" size='large' onClick={handleClicker}>Hello World</StyledButton>
+	);
+
+	it('snapshot test', () => {
+		setUp();
+		expect(screen.getByRole('button')).toMatchSnapshot();
+	});
+
+	it('find button in document by role', () => {
+		setUp();
+		expect(screen.getByRole('button')).toBeInTheDocument();
+	});
+
+	it('not find button by text', () => {
+		setUp();
+		expect(screen.queryByText(/another/i)).toBeNull();
+	});
+
+	it('find button by text', () => {
+		setUp();
+		expect(screen.getByText(/hello/i)).toBeInTheDocument();
+	});
+
+	it('click button', () => {
+		setUp();
+		fireEvent.click(screen.getByRole('button'));
+		expect(handleClicker).toHaveBeenCalledTimes(1);
+	});
+
+	screen.debug();
+
+})
