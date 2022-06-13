3f992a0f4444ef757d6e7a55e1450ffa691da882
SVA2021 <sofronov.vit@gmail.com>
2022-06-13 11:54:11 +0400

    upd: svg icon description simplifu

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,26 +1,17 @@
 import { LocalMovies } from '@mui/icons-material';
 import { Typography } from '@mui/material';
-import { theme } from '../../theme';
 
 const Logo = () => {
 	return (
 		<Typography data-testid='logo' variant='h1' component={'p'} sx={{
 			display: 'flex',
 			alignItems: 'center',
-			color: theme.palette.text.secondary,
+			flexWrap: 'nowrap',
+			color: 'text.secondary',
 			textTransform: 'uppercase',
 		}}>
 			Movie
-			<LocalMovies sx={{
-				width: 'auto',
-				height: theme.typography.pxToRem(32),
-				[theme.breakpoints.up('lg')]: {
-					height: theme.typography.pxToRem(48),
-				},
-				[theme.breakpoints.up('xl')]: {
-					height: theme.typography.pxToRem(72),
-				},
-			}} />
+			<LocalMovies fontSize='large'/>
 			Finder
 		</Typography>
 	)
