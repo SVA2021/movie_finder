e405c51a9b19b80a838d5b0d9d67cc74242e013b
SVA2021 <sofronov.vit@gmail.com>
2022-06-13 14:40:59 +0400

    upd: header initial layout

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,34 @@
+import { AppBar, Box, Grid, Toolbar } from '@mui/material';
+import AccountMenu from '../AccountMenu/AccountMenu';
+import Logo from '../Logo/Logo';
+import MenuMain from '../MenuMain/MenuMain';
+import SearchMenu from '../SearchMenu/SearchMenu';
+
+const Header = () => {
+	return (
+		<Box sx={{ flexGrow: 1, bgcolor: 'primary.main' }}>
+			<AppBar position='sticky' sx={{ maxWidth: 'xl', m: '0 auto' }}>
+				<Toolbar sx={{ boxShadow: 'none' }}>
+					<Grid container justifyContent={'space-between'} alignItems={'center'}>
+						<Grid item xs={3} md={1}>
+							<MenuMain />
+						</Grid>
+						<Grid item xs={9} md={5} lg={4}>
+							<Logo />
+						</Grid>
+						<Grid item md={1} lg={5} xl={4}
+							sx={{ display: { xs: 'none', md: 'block' } }}>
+							<SearchMenu />
+						</Grid>
+						<Grid item xs={false} md={3} lg={2}
+							sx={{ display: { xs: 'none', md: 'block' } }}>
+							<AccountMenu />
+						</Grid>
+					</Grid>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
+}
+
+export default Header;
