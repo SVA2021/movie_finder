d53e3a3595a988af7516719fc2149da8bafb2d4c
SVA2021 <sofronov.vit@gmail.com>
2022-06-13 14:40:44 +0400

    upd: main menu initial layout

---
+++ src/components/MenuMain/MenuMain.tsx
@@ -0,0 +1,79 @@
+import { AccountCircle, Home, LocalMovies, Menu as MenuIcon, Movie } from '@mui/icons-material';
+import { Grid, Popover } from '@mui/material';
+import * as React from 'react';
+import SearchField from '../SearchField/SearchField';
+import StyledButton from '../Styled_Button/Styled_Button';
+import StyledTextItem from '../Styled_TextItem/Styled_TextItem';
+
+const MenuMain = (props: any) => {
+
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const open = Boolean(anchorEl);
+	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorEl(event.currentTarget);
+	};
+	const handleClose = () => {
+		setAnchorEl(null);
+	};
+
+	return (
+		<div>
+			<StyledButton
+				id="fade-button"
+				aria-controls={open ? 'fade-menu' : undefined}
+				aria-haspopup="true"
+				aria-expanded={open ? 'true' : undefined}
+				onClick={handleClick}
+			>
+				<MenuIcon fontSize='large' />
+			</StyledButton>
+			<Popover
+				id="main-menu"
+				anchorEl={anchorEl}
+				open={open}
+				onClose={handleClose}
+				anchorOrigin={{
+					vertical: 'bottom',
+					horizontal: 'left',
+				}}
+				transformOrigin={{
+					vertical: 'top',
+					horizontal: 'left',
+				}}
+			>
+				<Grid container direction={'column'} spacing={2} sx={{
+					bgcolor: 'primary.main',
+					p: 2,
+				}} >
+					<Grid item xs={12} md={6} lg={3}>
+						<StyledTextItem ><Home fontSize='medium' />Home</StyledTextItem>
+					</Grid>
+					<Grid item xs={12} md={6} lg={3}>
+						<StyledTextItem ><LocalMovies fontSize='medium' />Movies</StyledTextItem>
+					</Grid>
+					<Grid item xs={12} md={6} lg={3}>
+						<StyledTextItem ><Movie fontSize='medium' />Series</StyledTextItem>
+					</Grid>
+					<Grid item xs={12} sx={{
+						display: {
+							xs: 'block',
+							md: 'none',
+						}
+					}}>
+						<SearchField />
+					</Grid>
+					<Grid item xs={12} sx={{
+						display: {
+							xs: 'block',
+							md: 'none',
+						}
+					}}>
+						<StyledTextItem ><AccountCircle fontSize='medium' />Login</StyledTextItem>
+					</Grid>
+				</Grid>
+			</Popover>
+		</div>
+	);
+}
+
+export default MenuMain;
