7fb1014c5d14e4390e34f236843e93604dc262f4
SVA2021 <sofronov.vit@gmail.com>
2022-06-14 22:08:44 +0400

    upd: add Main (initial)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import { Container } from '@mui/system';
 import Banner from './components/Banner/Banner';
 import Header from './components/Header/Header';
+import Main from './components/Main/Main';
 import { theme } from './theme';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
 			<Header />
 			<Container maxWidth='xl'>
 				<Banner/>
+				<Main/>
 			</Container>
 		</ThemeProvider>
 	);
