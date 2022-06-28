1257e1342c5fea02fbab7353e2570b05b9e9d958
SVA2021 <sofronov.vit@gmail.com>
2022-06-28 21:03:23 +0400

    upd: finish smallcard with tests

--- src/components/SmallCard/SmallCard.spec.tsx
+++ src/components/SmallCard/SmallCard.spec.tsx
@@ -1,21 +1,11 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import { filmDataShort } from '../../features/movieFinder/fakeData';
+import { fakeSmallCard, filmDataShort } from '../../features/movieFinder/fakeData';
 import SmallCard from './SmallCard';
-
-// const filmDataShort = {
-// 	filmId: 100,
-// 	nameRu: 'FakeNameRu',
-// 	nameEng: 'FakeNameEng',
-// 	nameOriginal: 'OriginalFakeName',
-// 	posterUrl: 'https://kinopoiskapiunofficial.tech/images/posters/kp/301.jpg',
-// 	posterUrlPreview: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/eed1de3a-5400-43b3-839e-22490389bf54/360',
-// 	relationType: 'SIMILAR',
-// }
 
 describe('render SmallCard', () => {
 
 	const handleClicker = jest.fn();
-	const setUp = () => render(<SmallCard {...filmDataShort} handleClick={ handleClicker()} />);
+	const setUp = () => render(<SmallCard item={fakeSmallCard} handleClick={ handleClicker()} />);
 
 	it('snapshot test', () => {
 		setUp();
@@ -32,7 +22,7 @@ describe('render SmallCard', () => {
 	it('img uses correct src', () => {
 		setUp();
 		const image = screen.getByAltText('FakeNameEng');
-		expect(image).toHaveAttribute('src', 'https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/eed1de3a-5400-43b3-839e-22490389bf54/360');
+		expect(image).toHaveAttribute('src', 'https://kinopoiskapiunofficial.tech/images/posters/kp_small/301.jpg');
 	});
 	it('click on Card', () => {
 		setUp();
--- src/components/SmallCard/SmallCard.tsx
+++ src/components/SmallCard/SmallCard.tsx
@@ -1,19 +1,41 @@
-import { Box, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
+import { convertNumToTime, convertRatingToColor } from "../../features/movieFinder/functions";
+import { TSmallCard } from "../../features/movieFinder/movieFinderTypes";
 
-const SmallCard = () => {
+const SmallCard = (props: { item: TSmallCard, handleClick: (n: number) => void }): JSX.Element => {
+
+	const { filmId, nameRu, nameEn, year, rating, filmLength, posterUrlPreview } = props.item;
+	const handleClick = props.handleClick;
+	const filmLengthString = convertNumToTime(filmLength);
+	const ratingColored = convertRatingToColor(rating);
+
 	return (
-		<Box sx={{
-			height: '100px',
-			display: 'flex',
-			alignItems: 'center',
-			justifyContent: 'center',
-			bgcolor: 'action.hover',
-			mt: 2,
-		}}>
-			<Typography variant="h1" component={'p'}>
-				here will be a banner
-			</Typography>
-		</Box>
+		<Card data-testid={'smallCard'} sx={{ position: 'relative' }}>
+			<CardActionArea onClick={() => handleClick(filmId)}>
+				<CardMedia
+					component="img"
+					width="100%"
+					image={posterUrlPreview}
+					alt={nameEn?.toString()}
+				/>
+				<CardContent>
+					<Typography variant="body1" component="p">{nameRu}</Typography>
+					<Typography variant="body1" component="p">{nameEn}</Typography>
+					<Typography variant="body1" component="p">{year}</Typography>
+					<Typography variant="body1" component="p">{filmLengthString}</Typography>
+					<Typography variant="h3" component="p" sx={{
+						position: 'absolute',
+						top: 0,
+						left: '-8px',
+						padding: '0px 12px',
+						backgroundColor: ratingColored,
+						color: 'common.white',
+					}}>
+						{rating}
+					</Typography>
+				</CardContent>
+			</CardActionArea>
+		</Card>
 	)
 }
 
