c654b06de923c9c8d38124e6e00c765995853b81
SVA2021 <sofronov.vit@gmail.com>
2022-07-01 19:45:33 +0400

    upd: add movieFinder slicer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import movieFinderReducer from '../features/movieFinder/movieFinderSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+		movieFinder: movieFinderReducer,
   },
 });
 
