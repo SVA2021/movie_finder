470920f82576c0e06a319f164316fb763b9b176e
SVA2021 <sofronov.vit@gmail.com>
2022-07-02 16:25:51 +0400

    upd: component optimized for difference data

--- src/components/SmallCard/SmallCard.tsx
+++ src/components/SmallCard/SmallCard.tsx
@@ -1,13 +1,23 @@
 import { Card, CardActionArea, CardContent, CardMedia, Typography } from "@mui/material";
-import { convertNumToTime, convertRatingToColor } from "../../features/movieFinder/functions";
-import { TSmallCard } from "../../features/movieFinder/movieFinderTypes";
+import { convertRatingToColor } from "../../features/movieFinder/functions";
+import { TSearchItem, TSmallCard } from "../../features/movieFinder/movieFinderTypes";
 
-const SmallCard = (props: { item: TSmallCard, handleClick: (n: number) => void }): JSX.Element => {
+interface TSmallCardProps {
+	item: TSmallCard | TSearchItem
+	handleClick: (n: number) => void
+}
 
-	const { filmId, nameRu, nameEn, year, rating, filmLength, posterUrlPreview } = props.item;
+const SmallCard = (props: TSmallCardProps): JSX.Element => {
+
+	const { nameRu, nameEn, year, posterUrlPreview } = props.item;
+
+	//properties below has difference type for same keys or different keys for same data
+	const filmId = ('filmId' in props.item) ? props.item.filmId : props.item.kinopoiskId;
+	const rating = ('rating' in props.item) ? props.item.rating : props.item.ratingKinopoisk;
+	const filmLength = ('filmLength' in props.item) ? props.item.filmLength : null;
+	
 	const handleClick = props.handleClick;
-	const filmLengthString = convertNumToTime(filmLength);
-	const ratingColored = convertRatingToColor(rating);
+	const ratingColored = convertRatingToColor(Number(rating));
 
 	return (
 		<Card data-testid={'smallCard'} sx={{ position: 'relative' }}>
@@ -16,17 +26,17 @@ const SmallCard = (props: { item: TSmallCard, handleClick: (n: number) => void }
 					component="img"
 					width="100%"
 					image={posterUrlPreview}
-					alt={nameEn?.toString()}
+					alt={nameRu?.toString() || ''}
 				/>
 				<CardContent>
 					<Typography variant="body1" component="p">{nameRu}</Typography>
-					<Typography variant="body1" component="p">{nameEn}</Typography>
+					{nameEn && <Typography variant="body1" component="p">{nameEn}</Typography>}
 					<Typography variant="body1" component="p">{year}</Typography>
-					<Typography variant="body1" component="p">{filmLengthString}</Typography>
+					{filmLength && <Typography variant="body1" component="p">{filmLength}</Typography>}
 					<Typography variant="h3" component="p" sx={{
 						position: 'absolute',
 						top: 0,
-						left: '-8px',
+						left: 0,
 						padding: '0px 12px',
 						backgroundColor: ratingColored,
 						color: 'common.white',
