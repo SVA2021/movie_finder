d6e04a1a33eef289f564502328729a6fcf27e4ce
SVA2021 <sofronov.vit@gmail.com>
2022-10-23 21:09:56 +0400

    upd: refactoring

--- src/features/movieFinder/functions.ts
+++ src/features/movieFinder/functions.ts
@@ -1,20 +1,16 @@
 export const normalizeDateString = (date: number): string => {
-	let result;
-	(date < 10) ? result = '0' + String(date) : result = String(date);
-	return result;
+	return (date < 10) ? '0' + String(date) : String(date);
 }
 
-export const convertNumToTime = (num: number | null): string => {
-	let result;
+export const getTimeFromNum = (num: number | null): string => {
 	if (num === null) return '';
 	let hours = Math.floor(num % 60);
 	let minutes = num % 60;
 
-	result = (hours === 0) ? `${minutes}мин ` : `${hours}ч ${minutes}мин`;
-	return result;
+	return (hours === 0) ? `${minutes}мин ` : `${hours}ч ${minutes}мин`;
 }
 
-export const convertRatingToColor = (num: number| null): string => {
+export const getColorFromRate = (num: number | null): string => {
 	if (num === null) return '';
 	if (num <= 0 || num > 10) return 'white';
 	if (num >= 8) return 'green';
