9dee7f0645ebef1dbe4bdf5c53f4cd0ebbf37669
SVA2021 <sofronov.vit@gmail.com>
2022-11-04 22:06:50 +0400

    add Home page initial

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,40 @@
-import { Box, Typography } from "@mui/material";
+import {Box, Skeleton} from "@mui/material";
+import {FC, useEffect} from 'react';
+import {useAppDispatch, useAppSelector} from "../../app/hooks";
+import {SmallCard} from "../../components";
+import {getTopListAsync, selectHomePage, selectHomePageCurrent, selectStatus} from "../../features/movieFinder/movieFinderSlice";
 
-const Home = () => {
+export const Home: FC<any> = () => {
+
+	const dispatch = useAppDispatch();
+	const SERVER_STATUS = useAppSelector(selectStatus);
+	const CurrentPage = useAppSelector(selectHomePageCurrent);
+	const HomePage = useAppSelector(selectHomePage);
+	const Films = HomePage === null ? null : HomePage[CurrentPage.type]
+	const CurrentFilms = Films === null ? [] : Films[CurrentPage.page]
+
+	useEffect(() => {
+		dispatch(getTopListAsync(CurrentPage));
+	}, [CurrentPage, dispatch,]);
+
 	return (
 		<Box sx={{
-			height: '100px',
+			m: 2,
 			display: 'flex',
+			flexDirection: 'row',
 			alignItems: 'center',
 			justifyContent: 'center',
-			bgcolor: 'action.hover',
-			mt: 2,
 		}}>
-			<Typography variant="h1" component={'p'}>
-				here will be a banner
-			</Typography>
+			<Box display={'grid'} gridTemplateColumns={'repeat(12, 1fr)'} gap={3}>
+				{
+					SERVER_STATUS === 'loading'
+						? <Skeleton />
+						: CurrentFilms.map((item) =>
+							<Box gridColumn={{xs: 'span 12', md: 'span 3', lg: 'span 2'}} key={item.id} >
+								<SmallCard item={item} />
+							</Box>
+						)}
+			</Box>
 		</Box>
 	)
-}
-
-export default Home;
+};
