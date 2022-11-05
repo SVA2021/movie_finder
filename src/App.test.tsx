3b6158c1c8a483102695f87a60fbb8e4a453419f
SVA2021 <sofronov.vit@gmail.com>
2022-11-05 14:05:53 +0400

    upd App import

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-import App from './App';
+import {App} from './App';
 
 test('renders Hello World button', () => {
 	render(
--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
-import {CssBaseline, ThemeProvider} from '@mui/material';
+import {CssBaseline, Skeleton, ThemeProvider} from '@mui/material';
 import {Container} from '@mui/system';
+import {useAppSelector} from './app/hooks';
 import {Banner, Header, Main} from './components';
+import {selectStatus} from './features/movieFinder/movieFinderSlice';
 import {theme} from './theme';
 
-function App() {
+export const App = () => {
+	const SERVER_STATUS = useAppSelector(selectStatus);
+
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
 			<Header />
 			<Container maxWidth='xl'>
-				<Banner />
-				<Main />
+				{/* <Banner /> */}
+				{
+					SERVER_STATUS === 'loading'
+						? <Skeleton />
+						: <Main />
+				}
 			</Container>
 		</ThemeProvider>
 	);
-}
-
-export default App;
+};
