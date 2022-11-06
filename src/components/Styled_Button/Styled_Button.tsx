d0fb80125d4f3a7d98c8756bc03c1c80cdf1731f
SVA2021 <sofronov.vit@gmail.com>
2022-11-06 13:06:00 +0400

    add shadow

--- src/components/Styled_Button/Styled_Button.tsx
+++ src/components/Styled_Button/Styled_Button.tsx
@@ -1,26 +1,29 @@
-import { Button, ButtonProps, useTheme } from '@mui/material';
+import {Button, ButtonProps, useTheme} from '@mui/material';
 
 export const StyledButton = (props: ButtonProps) => {
 	const theme = useTheme();
 
 	return (
-			<Button {...props} sx={{
-				borderRadius: theme.shape.borderRadius,
-				color: theme.palette.text.secondary,
-				"&:hover": {
-					color: theme.palette.action.hover,
-					boxShadow: `8px 8px 8px ${theme.palette.action.hover}`,
-				},
-				"&:active": {
-					color: theme.palette.action.active,
-					boxShadow: `8px 8px 8px ${theme.palette.action.active}`,
-				},
-				"&:focus": {
-					color: theme.palette.common.white,
-					boxShadow: `8px 8px 8px ${theme.palette.action.focus}`,
-				},
-			}}
-			/>
+		<Button {...props} sx={{
+			borderRadius: theme.shape.borderRadius,
+			color: theme.palette.text.secondary,
+			"&:hover": {
+				color: theme.palette.action.hover,
+				boxShadow: `8px 8px 8px ${theme.palette.action.hover},
+					-8px -8px 8px ${theme.palette.action.hover}`,
+			},
+			"&:active": {
+				color: theme.palette.action.active,
+				boxShadow: `8px 8px 8px ${theme.palette.action.hover},
+					-8px -8px 8px ${theme.palette.action.hover}`,
+			},
+			"&:focus": {
+				color: theme.palette.common.white,
+				boxShadow: `8px 8px 8px ${theme.palette.action.hover},
+					-8px -8px 8px ${theme.palette.action.hover}`,
+			},
+		}}
+		/>
 	)
 };
 StyledButton.muiName = 'Button';
