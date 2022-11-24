9b2918c3dc5f91176585242623d46f4f8738771e
SVA2021 <sofronov.vit@gmail.com>
2022-11-24 11:26:27 +0400

    add swiper list hoc component

---
+++ src/components/SwipeListTemplate/SwipeListTemplate.tsx
@@ -0,0 +1,59 @@
+import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import {Grid, useTheme} from '@mui/material';
+import {FC, useRef} from 'react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {SmallCard} from '../SmallCard/SmallCard';
+import {StyledButton} from '../Styled_Button/Styled_Button';
+import {SwipeListTemplateProps} from './SwiperListTemplate.types';
+
+export const SwipeListTemplate: FC<SwipeListTemplateProps> = ({data}) => {
+    const theme = useTheme();
+    const swiperRef = useRef<any>();
+
+    return (
+        <Grid container spacing={1} direction={'row'} alignItems={'center'} justifyContent={'space-between'}
+            sx={{bgcolor: 'primary.main'}}
+        >
+            <Grid item lg={1} sx={{display: {xs: 'none', lg: 'flex', }, justifyContent: 'center', }}>
+                <StyledButton onClick={() => swiperRef.current.slidePrev()}>
+                    <NavigateBeforeIcon fontSize='large' />
+                </StyledButton>
+            </Grid>
+            <Grid item xs={12} lg={10} >
+                <Swiper
+                    onSwiper={(swiper) => swiperRef.current = swiper}
+                    slidesPerView={1}
+                    spaceBetween={8}
+                    breakpoints={
+                        {
+                            [theme.breakpoints.values.md]: {
+                                slidesPerView: 4,
+                                spaceBetween: 16,
+                            },
+                            [theme.breakpoints.values.lg]: {
+                                slidesPerView: 6,
+                                spaceBetween: 24,
+                            },
+                        }
+                    }
+                >
+                    {
+                        data.map((item, index) =>
+                            <SwiperSlide key={index} >
+                                <SmallCard item={item} />
+                            </SwiperSlide>
+                        )
+                    }
+                </Swiper>
+            </Grid>
+            <Grid item lg={1} sx={{display: {xs: 'none', lg: 'flex', }, justifyContent: 'center', }}>
+                <StyledButton onClick={() => swiperRef.current.slideNext()}>
+                    <NavigateNextIcon fontSize='large' />
+                </StyledButton>
+            </Grid>
+        </Grid>
+    );
+};
