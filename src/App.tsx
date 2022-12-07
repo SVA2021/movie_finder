b0a7b14a04ac6f0fe71a6935179438901b4dda9e
SVA2021 <sofronov.vit@gmail.com>
2022-12-07 19:02:35 +0400

    add base routing to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import {CssBaseline, ThemeProvider} from '@mui/material';
-import {Header, Main} from './layout';
+import {Route, Routes} from 'react-router-dom';
+import {Layout} from './layout';
+import {Home} from './pages';
 import {theme} from './theme';
+import 'swiper/css';
 
 export const App = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<CssBaseline />
-			<Header />
-			<Main />
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
 		</ThemeProvider>
-	);
+	)
 };
