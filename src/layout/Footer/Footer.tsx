39690ba197740b80bd0252d2b7f4c7b195816ac2
SVA2021 <sofronov.vit@gmail.com>
2022-12-07 18:59:13 +0400

    footer initial commit

---
+++ src/layout/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import {Box} from '@mui/material';
+
+export const Footer = () => {
+	return (
+		<Box
+			sx={{
+				flexGrow: 1,
+				bgcolor: 'primary.main',
+				color: 'common.white',
+				textAlign: 'center',
+			}}>
+			SVA	2022
+		</Box>
+	)
+};
