842382a368b8e5be25874dab0ccb039c6fe9d065
SVA2021 <sofronov.vit@gmail.com>
2022-12-07 19:00:46 +0400

    base layout of pages initial commit

---
+++ src/layout/Layout/Layout.tsx
@@ -0,0 +1,25 @@
+import {Backdrop, CircularProgress, Container} from "@mui/material";
+import {Outlet} from "react-router-dom";
+import {useAppSelector} from "../../app/hooks";
+import {selectStatus} from "../../features/movieFinder/movieFinderSlice";
+import {Footer, Header} from "../index";
+
+export const Layout = () => {
+    const SERVER_STATUS = useAppSelector(selectStatus);
+
+    return (
+        <Container
+            maxWidth='xl'
+            sx={{
+                minHeight: '100vh',
+                bgcolor: 'primary.main',
+            }} >
+            <Backdrop open={SERVER_STATUS === 'loading'} >
+                <CircularProgress color="warning" />
+            </Backdrop>
+            <Header />
+            <Outlet />
+            <Footer />
+        </Container>
+    )
+};
