2944bf4beff10245b630e444ea9ba3e48489430a
SVA2021 <sofronov.vit@gmail.com>
2022-12-09 10:51:26 +0400

    upd rating convertation

--- src/utils.ts
+++ src/utils.ts
@@ -12,15 +12,21 @@ export const getTimeFromMinutes = (num: number | null): string => {
     return (hh === 0) ? `${mm}мин ` : `${hh}ч ${mm}мин`;
 }
 
-export const getColorFromRate = (numberInString: string | null): string => {
-    const num = numberInString?.includes('%')
-        ? parseFloat(numberInString) / 10
-        : Number(numberInString);
+export const getColorFromRate = (num: number | null): string => {
     if (num === null) return '';
     if (num <= 0 || num > 10) return 'white';
     if (num >= 8) return 'green';
     if (num >= 6) return 'orange';
     return 'red';
+}
+
+export const getRateFromString = (numberInString: number | string | null | undefined): number | null => {
+    if (numberInString === null || !numberInString) return null;
+    return typeof (numberInString) === 'string'
+        ? numberInString?.includes('%')
+            ? parseFloat(numberInString) / 10
+            : Number(numberInString)
+        : numberInString
 }
 
 export function normalizeTimeString(time: string | number | null): string | null {
