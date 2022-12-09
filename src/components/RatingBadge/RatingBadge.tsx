9fea33a8191f13d82d61a684fb89f7d9c8510e3c
SVA2021 <sofronov.vit@gmail.com>
2022-12-09 10:51:54 +0400

    upd Rating functions , add position

--- src/components/RatingBadge/RatingBadge.tsx
+++ src/components/RatingBadge/RatingBadge.tsx
@@ -1,22 +1,26 @@
 import {Typography} from "@mui/material";
 import {FC} from "react";
+import {getColorFromRate, getRateFromString} from "../../utils";
 
 interface RatingBadgeProps {
     rating: number | string | null | undefined
-    bgColor: string
+    position: 'left' | 'right'
 }
 
-export const RatingBadge: FC<RatingBadgeProps> = ({rating, bgColor}) => {
+export const RatingBadge: FC<RatingBadgeProps> = ({rating, position}) => {
+    const normalizeRating = getRateFromString(rating);
+    const bgColor = getColorFromRate(normalizeRating);
     return (
         <Typography variant="h3" component="p" sx={{
             position: 'absolute',
             top: 0,
-            left: 0,
+            left: position === 'left' ? 0 : null,
+            right: position === 'right' ? 0 : null,
             padding: '0px 12px',
             backgroundColor: bgColor,
             color: 'common.white',
         }}>
-            {rating}
+            {normalizeRating?.toFixed(1)}
         </Typography>
     )
 };
