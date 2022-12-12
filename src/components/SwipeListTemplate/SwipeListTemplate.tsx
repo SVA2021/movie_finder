54ae36b0672be541bcb80e1e36757de19256b0a1
SVA2021 <sofronov.vit@gmail.com>
2022-12-12 12:54:22 +0400

    add empty data fallback

--- src/components/SwipeListTemplate/SwipeListTemplate.tsx
+++ src/components/SwipeListTemplate/SwipeListTemplate.tsx
@@ -1,6 +1,6 @@
 import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
 import NavigateNextIcon from '@mui/icons-material/NavigateNext';
-import {Grid, useTheme} from '@mui/material';
+import {Grid, Typography, useTheme} from '@mui/material';
 import {FC, useRef} from 'react';
 import 'swiper/css';
 import {Swiper, SwiperSlide} from 'swiper/react';
@@ -32,31 +32,43 @@ export const SwipeListTemplate: FC<SwipeListTemplateProps> = ({data}) => {
                 </StyledButton>
             </Grid>
             <Grid item xs={12} lg={10} >
-                <Swiper
-                    onSwiper={(swiper) => swiperRef.current = swiper}
-                    slidesPerView={1}
-                    spaceBetween={8}
-                    breakpoints={
-                        {
-                            [theme.breakpoints.values.md]: {
-                                slidesPerView: 4,
-                                spaceBetween: 16,
-                            },
-                            [theme.breakpoints.values.lg]: {
-                                slidesPerView: 5,
-                                spaceBetween: 24,
-                            },
+                {data.length > 0
+                    ? <Swiper
+                        onSwiper={(swiper) => swiperRef.current = swiper}
+                        slidesPerView={1}
+                        spaceBetween={8}
+                        breakpoints={
+                            {
+                                [theme.breakpoints.values.md]: {
+                                    slidesPerView: 4,
+                                    spaceBetween: 16,
+                                },
+                                [theme.breakpoints.values.lg]: {
+                                    slidesPerView: 5,
+                                    spaceBetween: 24,
+                                },
+                            }
                         }
-                    }
-                >
-                    {
-                        data.map((item, index) =>
-                            <SwiperSlide key={index} >
-                                <SmallCard item={item} />
-                            </SwiperSlide>
-                        )
-                    }
-                </Swiper>
+                    >
+                        {
+                            data.map((item, index) =>
+                                <SwiperSlide key={index} >
+                                    <SmallCard item={item} />
+                                </SwiperSlide>
+                            )
+                        }
+                    </Swiper>
+                    :
+                    <Typography
+                        ml={{lg: 15, xl: 20}}
+                        gutterBottom
+                        variant="h3"
+                        component={'p'}
+                        fontStyle={'italic'}
+                    >
+                        Нет данных
+                    </Typography>
+                }
             </Grid>
             <Grid item lg={1}
                 sx={{
