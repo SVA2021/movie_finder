816ed94b038dd2961381931e4df068019978d6de
SVA2021 <sofronov.vit@gmail.com>
2022-12-12 11:12:15 +0400

    add links to homepage

--- src/components/MenuMain/MenuMain.tsx
+++ src/components/MenuMain/MenuMain.tsx
@@ -2,18 +2,25 @@ import {Home, LocalMovies, Menu as MenuIcon, Movie} from '@mui/icons-material';
 import {Grid, Link, Popover} from '@mui/material';
 import * as React from 'react';
 import {StyledButton, StyledTextItem, SubMenuMobile, } from '../../components';
-import {Link as RouterLink} from "react-router-dom";
+import {Link as RouterLink, useNavigate} from "react-router-dom";
 
 export const MenuMain = (props: any) => {
 
+	const navigate = useNavigate();
+
 	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
 	const open = Boolean(anchorEl);
-	const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+	const handleOpen = (event: React.MouseEvent<HTMLElement>) => {
 		setAnchorEl(event.currentTarget);
 	};
 	const handleClose = () => {
 		setAnchorEl(null);
 	};
+
+	function handleClick(path: string): void {
+		navigate(path);
+		handleClose();
+	}
 
 	return (
 		<>
@@ -22,7 +29,7 @@ export const MenuMain = (props: any) => {
 				aria-controls={open ? 'fade-menu' : undefined}
 				aria-haspopup="true"
 				aria-expanded={open ? 'true' : undefined}
-				onClick={handleClick}
+				onClick={handleOpen}
 			>
 				<MenuIcon fontSize='large' />
 			</StyledButton>
@@ -44,10 +51,8 @@ export const MenuMain = (props: any) => {
 					bgcolor: 'primary.main',
 					p: 2,
 				}} >
-					<Grid item xs={12} md={6} lg={3}>
-						<Link component={RouterLink} to={'/'} >
-							<StyledTextItem ><Home fontSize='medium' />Home</StyledTextItem>
-						</Link>
+					<Grid item xs={12} md={6} lg={3} onClick={() => handleClick('/')} >
+						<StyledTextItem ><Home fontSize='medium' />Home</StyledTextItem>
 					</Grid>
 					<Grid item xs={12} md={6} lg={3}>
 						<StyledTextItem ><LocalMovies fontSize='medium' />Movies</StyledTextItem>
