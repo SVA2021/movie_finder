b97d7556d7dac77c461761a23fe9e531a994982a
SVA2021 <sofronov.vit@gmail.com>
2022-12-22 00:33:35 +0400

    add navigate func for search field

--- src/components/SearchField/SearchField.tsx
+++ src/components/SearchField/SearchField.tsx
@@ -1,17 +1,26 @@
 import {Search} from '@mui/icons-material';
 import {Box, IconButton, Input, InputAdornment} from '@mui/material';
 import {useRef} from 'react';
-import {useNavigate} from 'react-router-dom';
+import {createSearchParams, useNavigate} from 'react-router-dom';
+import {useAppDispatch} from '../../app/hooks';
+import {setSearchKeyword} from '../../features/movieFinder/movieFinderSlice';
 
 export const SearchField = () => {
     const ref = useRef<HTMLInputElement | undefined>();
     const navigate = useNavigate();
+    const dispatch = useAppDispatch();
 
     function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-        if (!ref || !ref.current) return false;
         e.preventDefault();
-        // navigate('/search');
-        console.log(ref.current.value)
+        if (!ref || !ref.current) return false;
+        const keyword = ref.current.value;
+        const path = {
+            pathname: '/search',
+            search: createSearchParams({keyword}).toString(),
+        };
+        navigate(path);
+        dispatch(setSearchKeyword({keyword, page: 1}));
+        ref.current.value = '';
     }
 
     return (
