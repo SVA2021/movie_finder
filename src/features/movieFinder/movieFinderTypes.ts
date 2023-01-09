dc3bcc22471d09c777e4c661c6b8d7e7e6a4c368
SVA2021 <sofronov.vit@gmail.com>
2023-01-09 15:53:47 +0400

    search types updated

--- src/features/movieFinder/movieFinderTypes.ts
+++ src/features/movieFinder/movieFinderTypes.ts
@@ -17,135 +17,135 @@ export type TStatus = 'idle' | 'loading' | 'failed';
 //********** ITEM TYPES *************************/
 
 export interface TSmallCard {
-    id: number
-    nameRu: string | null
-    nameEn: string | null
-    year: string | number | null
-    filmLength: string | number | null
-    rating: string | null
-    posterUrlPreview: string
+  id: number
+  nameRu: string | null
+  nameEn: string | null
+  year: string | number | null
+  filmLength: string | number | null
+  rating: string | null
+  posterUrlPreview: string
 }
 
 export interface Film {
-    filmId: number
-    nameRu: string | null
-    nameEn: string | null
-    year: string | null
-    filmLength: string | null
-    countries: TCountry[]
-    genres: TGenre[]
-    rating: string | null
-    ratingVoteCount: number | null
-    posterUrl: string
-    posterUrlPreview: string
+  filmId: number
+  nameRu: string | null
+  nameEn: string | null
+  year: string | null
+  filmLength: string | null
+  countries: TCountry[]
+  genres: TGenre[]
+  rating: string | null
+  ratingVoteCount: number | null
+  posterUrl: string
+  posterUrlPreview: string
 }
 
 export interface TTopFilm {
-    filmId: number
-    nameRu: string | null
-    nameEn: string | null
-    year: string | null
-    filmLength: string | null
-    countries: TCountry[]
-    genres: TGenre[]
-    rating: string | null
-    ratingVoteCount: number | null
-    posterUrl: string
-    posterUrlPreview: string
-    ratingChange?: null | any
+  filmId: number
+  nameRu: string | null
+  nameEn: string | null
+  year: string | null
+  filmLength: string | null
+  countries: TCountry[]
+  genres: TGenre[]
+  rating: string | null
+  ratingVoteCount: number | null
+  posterUrl: string
+  posterUrlPreview: string
+  ratingChange?: null | any
 }
 
 export interface TFullCard {
-    kinopoiskId: number
-    imdbId: string | null
-    nameRu: string | null
-    nameEn: string | null
-    nameOriginal: string | null
-    posterUrl: string
-    posterUrlPreview: string
-    coverUrl: string | null
-    logoUrl: string | null
-    reviewsCount: number
-    ratingGoodReview: number | null
-    ratingGoodReviewVoteCount: number | null
-    ratingKinopoisk: number | null
-    ratingKinopoiskVoteCount: number | null
-    ratingImdb: number | null
-    ratingImdbVoteCount: number | null
-    ratingFilmCritics: number | null
-    ratingFilmCriticsVoteCount: number | null
-    ratingAwait: number | null
-    ratingAwaitCount: number | null
-    ratingRfCritics: number | null
-    ratingRfCriticsVoteCount: number | null
-    webUrl: string
-    year: number | null
-    filmLength: number | null
-    slogan: string | null
-    description: string | null
-    shortDescription: string | null
-    editorAnnotation: string | null
-    isTicketsAvailable: boolean
-    productionStatus: TProductionStatus | null
-    type: TFilmType
-    ratingMpaa: string | null
-    ratingAgeLimits: string | null
-    countries: TCountry[]
-    genres: TGenre[]
-    startYear?: number | null
-    endYear?: number | null
-    serial?: boolean | null
-    shortFilm?: boolean | null
-    completed?: boolean | null
-    hasImax?: boolean | null
-    has3D?: boolean | null
-    lastSync?: string
+  kinopoiskId: number
+  imdbId: string | null
+  nameRu: string | null
+  nameEn: string | null
+  nameOriginal: string | null
+  posterUrl: string
+  posterUrlPreview: string
+  coverUrl: string | null
+  logoUrl: string | null
+  reviewsCount: number
+  ratingGoodReview: number | null
+  ratingGoodReviewVoteCount: number | null
+  ratingKinopoisk: number | null
+  ratingKinopoiskVoteCount: number | null
+  ratingImdb: number | null
+  ratingImdbVoteCount: number | null
+  ratingFilmCritics: number | null
+  ratingFilmCriticsVoteCount: number | null
+  ratingAwait: number | null
+  ratingAwaitCount: number | null
+  ratingRfCritics: number | null
+  ratingRfCriticsVoteCount: number | null
+  webUrl: string
+  year: number | null
+  filmLength: number | null
+  slogan: string | null
+  description: string | null
+  shortDescription: string | null
+  editorAnnotation: string | null
+  isTicketsAvailable: boolean
+  productionStatus: TProductionStatus | null
+  type: TFilmType
+  ratingMpaa: string | null
+  ratingAgeLimits: string | null
+  countries: TCountry[]
+  genres: TGenre[]
+  startYear?: number | null
+  endYear?: number | null
+  serial?: boolean | null
+  shortFilm?: boolean | null
+  completed?: boolean | null
+  hasImax?: boolean | null
+  has3D?: boolean | null
+  lastSync?: string
 }
 
 export type TSearchItem = Pick<TFullCard, 'kinopoiskId' | 'imdbId' | 'nameRu' | 'nameEn' | 'nameOriginal'
-    | 'countries' | 'genres' | 'ratingKinopoisk' | 'ratingImdb' | 'year' | 'type' | 'posterUrl' | 'posterUrlPreview'>;
+  | 'countries' | 'genres' | 'ratingKinopoisk' | 'ratingImdb' | 'year' | 'type' | 'posterUrl' | 'posterUrlPreview'>;
 
 //********** RESPONSE TYPES *************************/
 
 export interface TSearchResponse {
-    total: number
-    totalPages: number
-    items: TSearchItem[]
+  total: number
+  totalPages: number
+  items: TSearchItem[]
 }
 
 export interface TSearchResponseData {
-    total: number | null
-    totalPages: number | null
-    currentPage: number
-    keyword: string | null
-    items: {[key: number]: TSearchItem[]}
+  total: number | null
+  totalPages: number | null
+  currentPage: number
+  keyword: string | null
+  items: {[key: number]: TSearchItem[]}
 }
 
 export interface TTopResponse {
-    pagesCount: number
-    films: TTopFilm[]
-    type: TTopList
-    page: number
+  pagesCount: number
+  films: TTopFilm[]
+  type: TTopList
+  page: number
 }
 
 export interface TTopData {
-    pagesCount: number
-    page: number
-    data: TSmallCard[]
+  pagesCount: number
+  page: number
+  data: TSmallCard[]
 }
 
 export interface THomePageCurrent {
-    type: TTopList
-    page: number
+  type: TTopList
+  page: number
 }
 
 export interface TMovieDetails {
-    movie: TFullCard | null
-    similars: TSimilarsData | null
-    videos: TVideosRes | null
-    images: TImagesRes | null
-    awards: TAwardsRes | null
-    facts: TFactsRes | null
+  movie: TFullCard | null
+  similars: TSimilarsData | null
+  videos: TVideosRes | null
+  images: TImagesRes | null
+  awards: TAwardsRes | null
+  facts: TFactsRes | null
 }
 
 // export type TMovieExtraType = 'similars' | 'videos' | 'images' | 'awards' | 'facts'
@@ -153,83 +153,101 @@ export type TMovieExtraType = 'videos' | 'images' | 'awards' | 'facts'
 export type TVideoItemType = 'YOUTUBE' | 'KINOPOISK_WIDGET' | 'UNKNOWN'
 
 export interface TMovieSimilarsRes {
-    total: number
-    items: Film[]
+  total: number
+  items: Film[]
 }
 
 export interface TSimilarsData {
-    total: number
-    data: TSmallCard[]
+  total: number
+  data: TSmallCard[]
 }
 
 export interface TVideoItem {
-    url: string
-    name: string
-    site: TVideoItemType
+  url: string
+  name: string
+  site: TVideoItemType
 }
 
 export interface TImageItem {
-    imageUrl: string
-    previewUrl: string
+  imageUrl: string
+  previewUrl: string
 }
 
 export interface TVideosRes {
-    total: number
-    items: TVideoItem[]
+  total: number
+  items: TVideoItem[]
 }
 
 export interface TImagesRes {
-    total: number
-    totalPages: number
-    items: TImageItem[]
+  total: number
+  totalPages: number
+  items: TImageItem[]
 }
 
 export interface TAwardsItem {
-    name: string
-    win: boolean
-    imageUrl: string
-    nominationName: string
-    year: number
+  name: string
+  win: boolean
+  imageUrl: string
+  nominationName: string
+  year: number
 }
 
 export interface TAwardsRes {
-    total: number
-    items: TAwardsItem[]
+  total: number
+  items: TAwardsItem[]
 }
 
 export interface TFactsItem {
-    text: string
-    spoiler: boolean
-    type: 'FACT' | 'BLOOPER'
+  text: string
+  spoiler: boolean
+  type: 'FACT' | 'BLOOPER'
 }
 
 export interface TFactsRes {
-    total: number
-    items: TFactsItem[]
+  total: number
+  items: TFactsItem[]
 }
 
 export interface TMovieExtraProps {
-    id: number
+  id: number
 }
 
 export interface TSearchRequest {
-    keyword: string | null
-    page: number
-    countries?: number[]
-    genres?: number[]
-    order?: TOrder
-    type?: TFilmType
-    ratingFrom?: number
-    ratingTo?: number
-    yearFrom?: number
-    yearTo?: number
+  keyword: string | null
+  page: number
+  // extended: TSearchExtended | null
 }
 
-export type TOrder = 'RATING' | 'NUM_VOTE' | 'YEAR'
+export const ORDERS = ['RATING', 'NUM_VOTE', 'YEAR'] as const;
+export type TOrder = typeof ORDERS[number];
+// export type TOrder = 'RATING' | 'NUM_VOTE' | 'YEAR'
 export type TFilterGenre = {id: number, genre: string}
 export type TFilterCountry = {id: number, country: string}
 
 export interface TFiltersResponse {
-    genres: TFilterGenre[]
-    countries: TFilterCountry[]
+  genres: TFilterGenre[]
+  countries: TFilterCountry[]
+}
+
+export interface TSearchForm {
+  countries: number | 'default'
+  genres: number | 'default'
+  order: TOrder
+  rating: [number, number]
+  year: [number, number]
+}
+
+export interface TSearchExtended {
+  countries: number[]
+  genres: number[]
+  order: TOrder
+  ratingFrom: number
+  ratingTo: number
+  yearFrom: number
+  yearTo: number
+}
+
+export interface TSearch {
+  search: TSearchRequest
+  extended: TSearchForm | null
 }
