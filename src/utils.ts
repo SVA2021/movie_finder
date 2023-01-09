bf62f4ed6de64901afe3bed6d89d9b6ebe74d631
SVA2021 <sofronov.vit@gmail.com>
2023-01-09 15:51:20 +0400

    add convert search filter function

--- src/utils.ts
+++ src/utils.ts
@@ -1,53 +1,66 @@
-import {TTopList} from "./features/movieFinder/movieFinderTypes";
+import {TSearchExtended, TSearchForm, TTopList} from "./features/movieFinder/movieFinderTypes";
 
 export const normalizeDateString = (date: number): string => {
-    return (date < 10) ? '0' + String(date) : String(date);
+  return (date < 10) ? '0' + String(date) : String(date);
 }
 
 export const getTimeFromMinutes = (num: number | null | undefined): string => {
-    if (!num) return '';
-    let hh = Math.floor(num / 60);
-    let mm = num % 60;
+  if (!num) return '';
+  let hh = Math.floor(num / 60);
+  let mm = num % 60;
 
-    return (hh === 0) ? `${mm}мин ` : `${hh}ч ${mm}м`;
+  return (hh === 0) ? `${mm}мин ` : `${hh}ч ${mm}м`;
 }
 
 export const getColorFromRate = (num: number | null): string => {
-    if (num === null) return '';
-    if (num <= 0 || num > 10) return 'white';
-    if (num >= 8) return 'green';
-    if (num >= 6) return 'orange';
-    return 'red';
+  if (num === null) return '';
+  if (num <= 0 || num > 10) return 'white';
+  if (num >= 8) return 'green';
+  if (num >= 6) return 'orange';
+  return 'red';
 }
 
 export const getRateFromString = (numberInString: number | string | null | undefined): number | null => {
-    if (numberInString === null || !numberInString) return null;
-    return typeof (numberInString) === 'string'
-        ? numberInString?.includes('%')
-            ? parseFloat(numberInString) / 10
-            : Number(numberInString)
-        : numberInString
+  if (numberInString === null || !numberInString) return null;
+  return typeof (numberInString) === 'string'
+    ? numberInString?.includes('%')
+      ? parseFloat(numberInString) / 10
+      : Number(numberInString)
+    : numberInString
 }
 
 export function normalizeTimeString(time: string | number | null): string | null {
-    if (time === null || time === '') return null;
-    const hh = String(time).slice(0, 2);
-    const mm = String(time).slice(3, 5);
-    return `${hh}ч${mm}м`
+  if (time === null || time === '') return null;
+  const hh = String(time).slice(0, 2);
+  const mm = String(time).slice(3, 5);
+  return `${hh}ч${mm}м`
 }
 
 export function getTopListTitle(topFilmType: TTopList): string {
-    switch (topFilmType) {
-        case 'TOP_100_POPULAR_FILMS':
-            return '100 самых популярных фильмов'
-        case 'TOP_250_BEST_FILMS':
-            return 'топ 250 фильмов кинопоиска'
-        case 'TOP_AWAIT_FILMS':
-            return 'самые ожидаемые фильмы'
-    }
+  switch (topFilmType) {
+    case 'TOP_100_POPULAR_FILMS':
+      return '100 самых популярных фильмов'
+    case 'TOP_250_BEST_FILMS':
+      return 'топ 250 фильмов кинопоиска'
+    case 'TOP_AWAIT_FILMS':
+      return 'самые ожидаемые фильмы'
+  }
 }
 
 export function getTopListType(type: string | undefined | null): TTopList {
-    if (type === 'TOP_100_POPULAR_FILMS' || type === 'TOP_250_BEST_FILMS' || type === 'TOP_AWAIT_FILMS') return type;
-    return 'TOP_100_POPULAR_FILMS';
+  if (type === 'TOP_100_POPULAR_FILMS' || type === 'TOP_250_BEST_FILMS' || type === 'TOP_AWAIT_FILMS') return type;
+  return 'TOP_100_POPULAR_FILMS';
+}
+
+export function convertSearchFilter(form: TSearchForm): TSearchExtended {
+  const {order, countries, genres, rating, year} = form;
+  return {
+    order,
+    countries: countries === 'default' ? [] : [countries],
+    genres: genres === 'default' ? [] : [genres],
+    ratingFrom: rating[0],
+    ratingTo: rating[1],
+    yearFrom: year[0],
+    yearTo: year[1]
+  }
 }
