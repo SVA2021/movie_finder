9c49f76c17a4c40eeb001662e05e76a58a9b4603
SVA2021 <sofronov.vit@gmail.com>
2023-01-10 16:24:43 +0400

    details refactoring

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,42 +1,43 @@
 import {Box, Tab, Tabs} from "@mui/material";
 import {FC, useState} from 'react';
-import {useParams} from "react-router-dom";
 import {AwardsList, FactsList, FullCard, ImgList, SimilarList, VideoList} from "../../components";
 import {TMovieExtraType} from "../../features/movieFinder/movieFinderTypes";
 
+
 export const Details: FC = () => {
 
-    const params = useParams();
-    const id = Number(params.id);
-    const [activeTab, setActiveTab] = useState<TMovieExtraType>('videos');
+  const [activeTab, setActiveTab] = useState<TMovieExtraType>('videos');
 
-    const handleChangeTabs = (event: React.SyntheticEvent, newValue: TMovieExtraType) => {
-        setActiveTab(newValue);
-    };
+  const SECTIONS = {
+    videos: <VideoList />,
+    images: <ImgList />,
+    awards: <AwardsList />,
+    facts: <FactsList />,
+  }
 
-    return (
-        <Box p={1}>
-            <FullCard id={id} />
-            <Box pt={{xs: 2, lg: 4}} color={'common.white'} >
-                <Tabs aria-label="tabs"
-                    value={activeTab}
-                    onChange={handleChangeTabs}
-                    indicatorColor="secondary"
-                    textColor="secondary"
-                >
-                    <Tab label="Videos" value={'videos'} />
-                    <Tab label="Images" value={'images'} />
-                    <Tab label="Awards" value={'awards'} />
-                    <Tab label="Facts" value={'facts'} />
-                </Tabs>
-                <Box mt={4} >
-                    {activeTab === 'videos' && <VideoList id={id} />}
-                    {activeTab === 'images' && <ImgList id={id} />}
-                    {activeTab === 'awards' && <AwardsList id={id} />}
-                    {activeTab === 'facts' && <FactsList id={id} />}
-                </Box>
-            </Box>
-            <SimilarList id={id} />
+  const handleChangeTabs = (event: React.SyntheticEvent, newValue: TMovieExtraType) => {
+    setActiveTab(newValue);
+  };
+
+  return (
+    <Box p={1}>
+      <FullCard />
+      <Box pt={{xs: 2, lg: 4}} color={'common.white'} >
+        <Tabs aria-label="tabs"
+          value={activeTab}
+          onChange={handleChangeTabs}
+          indicatorColor="secondary"
+          textColor="secondary"
+        >
+          {
+            Object.keys(SECTIONS).map((item) => <Tab key={item} label={item} value={item} />)
+          }
+        </Tabs>
+        <Box mt={4} >
+          {SECTIONS[activeTab]}
         </Box>
-    )
+      </Box>
+      <SimilarList />
+    </Box>
+  )
 };
