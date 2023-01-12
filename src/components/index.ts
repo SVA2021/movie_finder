0b25977693852ce1637016f350d2f6320da2e290
SVA2021 <sofronov.vit@gmail.com>
2023-01-12 18:10:41 +0400

    clean unused files

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,7 +5,6 @@ export * from './Logo/Logo';
 export * from './MenuMain/MenuMain';
 export * from './SearchField/SearchField';
 export * from './SearchMenu/SearchMenu';
-export * from './Slider/Slider';
 export * from './SmallCard/SmallCard';
 export * from './Styled_Button/Styled_Button';
 export * from './Styled_TextItem/Styled_TextItem';
