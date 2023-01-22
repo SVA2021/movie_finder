901b4bfb5994eb5bad04c25f76249aa13d28359f
SVA2021 <sofronov.vit@gmail.com>
2023-01-22 13:34:37 +0400

    upd code refactoring

--- src/components/SearchMenu/SearchMenu.tsx
+++ src/components/SearchMenu/SearchMenu.tsx
@@ -1,70 +1,56 @@
-import { Search } from '@mui/icons-material';
-import { Box, IconButton, Popover, useMediaQuery, useTheme } from '@mui/material';
+import {Search} from '@mui/icons-material';
+import {Box, IconButton, Popover, useMediaQuery, useTheme} from '@mui/material';
 import * as React from 'react';
 import {SearchField} from '../../components';
 
 export const SearchMenu = () => {
-	const theme = useTheme();
-	const isTablet = useMediaQuery(theme.breakpoints.only('md'));
+  const theme = useTheme();
+  const isTablet = useMediaQuery(theme.breakpoints.only('md'));
 
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	const open = Boolean(anchorEl);
-	const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
-	const handleClose = () => {
-		setAnchorEl(null);
-	};
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
-	return (
-		(isTablet)
-			? (
-				<>
-					<IconButton
-						id="search-button"
-						aria-controls={open ? 'search-menu' : undefined}
-						aria-haspopup="true"
-						aria-expanded={open ? 'true' : undefined}
-						onClick={handleClick}
-						sx={{
-							color: 'secondary.main',
-							// '&:hover': {
-							// 	color: 'action.hover',
-							// },
-							// '&:active': {
-							// 	color: 'action.active',
-							// },
-						}}
-					>
-						<Search fontSize='large' />
-					</IconButton>
-					<Popover
-						id="search-menu"
-						anchorEl={anchorEl}
-						open={open}
-						onClose={handleClose}
-						anchorOrigin={{
-							vertical: 'bottom',
-							horizontal: 'right',
-						}}
-						transformOrigin={{
-							vertical: 'top',
-							horizontal: 'right',
-						}}
-					>
-						<Box sx={{
-							bgcolor: 'primary.main',
-							p: 2,
-							width: 'calc(100vw - 32px)',
-							height: '200%',
-						}}>
-							<SearchField />
-						</Box>
-					</Popover>
-				</>
-			)
-			: (
-				<SearchField />
-			)
-	)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+
+  return (
+    (isTablet)
+      ? <>
+        <IconButton
+          id="search-button"
+          aria-controls={open ? 'search-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          color={'secondary'}
+        >
+          <Search fontSize='large' />
+        </IconButton>
+        <Popover
+          id="search-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <Box
+            sx={{
+              bgcolor: 'primary.main',
+              p: 2,
+              width: 'calc(100vw - 32px)',
+              height: '200%',
+            }}>
+            <SearchField />
+          </Box>
+        </Popover>
+      </>
+      : <SearchField />
+  )
 };
